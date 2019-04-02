5752c2a6d64a674725a422a830e46de87b99b9b2
Marlon Fuentes <marlonfuenteslp@gmail.com>
2019-04-01 18:29:59 -0600

    first push

---
+++ Assets/Scripts/buttonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonManager : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
+
---
+++ Assets/Scripts/musiquita.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musiquita : MonoBehaviour
+{
+
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+    public GameObject fire;
+    private bool flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        MusicSource.clip = MusicClip;
+
+        
+
+    }
+        // Update is called once per frame
+        void Update()
+        {
+        if (flag == true)
+        {
+            Invoke("apagar", 2);
+
+        }
+    }
+    public void yes() {
+        MusicSource.Play();
+    }
+    public void active() {
+        flag = true;
+        fire.SetActive(true);
+    }
+    public void apagar()
+    {
+        flag = false;
+        fire.SetActive(false);
+    }
+}   
